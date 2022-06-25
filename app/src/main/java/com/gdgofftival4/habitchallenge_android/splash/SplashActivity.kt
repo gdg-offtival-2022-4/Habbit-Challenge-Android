0ff912e7659b6d9f8ef6821425171a8ecaa5f367
winter223 <link5658@gmail.com>
2022-06-26 00:40:40 +0900

    스플래시

--- app/src/main/java/com/gdgofftival4/habitchallenge_android/splash/SplashActivity.kt
+++ app/src/main/java/com/gdgofftival4/habitchallenge_android/splash/SplashActivity.kt
@@ -3,22 +3,29 @@ package com.gdgofftival4.habitchallenge_android.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.gdgofftival4.habitchallenge_android.base.BaseBindingActivity
 import com.gdgofftival4.habitchallenge_android.common.HabitChallengeConfig
 import com.gdgofftival4.habitchallenge_android.databinding.ActivitySplashBinding
 import com.gdgofftival4.habitchallenge_android.home.HomeActivity
 import com.gdgofftival4.habitchallenge_android.login.LoginActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseBindingActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (HabitChallengeConfig.userId == null) {
-            startActivity(Intent(this, LoginActivity::class.java))
-        } else {
-            // Todo : 메인 화면 이동
-            startActivity(Intent(this, HomeActivity::class.java))
+        lifecycleScope.launch {
+            delay(2000L)
+            if (HabitChallengeConfig.userId == null) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+                finish()
+            }
         }
     }
 }
