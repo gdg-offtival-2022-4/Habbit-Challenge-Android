b70acae52ed02e118b7da972a3e195bf71f02b6d
winter223 <link5658@gmail.com>
2022-06-26 08:07:08 +0900

    메인 뷰 아이템 테두리 둥굴게

--- app/src/main/java/com/gdgofftival4/habitchallenge_android/home/adapter/RoomHolderPage.kt
+++ app/src/main/java/com/gdgofftival4/habitchallenge_android/home/adapter/RoomHolderPage.kt
@@ -1,13 +1,12 @@
 package com.gdgofftival4.habitchallenge_android.home.adapter
 
+import android.net.Uri
 import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.gdgofftival4.habitchallenge_android.R
 import com.gdgofftival4.habitchallenge_android.databinding.ItemRoomBinding
+import com.gdgofftival4.habitchallenge_android.extension.setCircleImageUri
 import com.gdgofftival4.habitchallenge_android.home.model.RoomUiResponse
 
 
@@ -40,22 +39,9 @@ class RoomHolderPage internal constructor(
         }
 
         // userImg
-        Glide.with(itemView.context)
-            .load(data.user_image_urls[0])
-            .transform(CenterCrop(), RoundedCorners(200))
-            .into(binding.userOne)
-
-        Glide.with(itemView.context)
-            .load(data.user_image_urls[0])
-            .transform(CenterCrop(), RoundedCorners(200))
-            .into(binding.userTwo)
-
-        Glide.with(itemView.context)
-            .load(data.user_image_urls[2])
-            .transform(CenterCrop(), RoundedCorners(200))
-            .into(binding.userThree)
-
-
+        binding.userOne.setCircleImageUri(Uri.parse(data.user_image_urls[0]), R.drawable.ic_profile_default)
+        binding.userTwo.setCircleImageUri(Uri.parse(data.user_image_urls[1]), R.drawable.ic_profile_default)
+        binding.userThree.setCircleImageUri(Uri.parse(data.user_image_urls[2]), R.drawable.ic_profile_default)
     }
 }
 
